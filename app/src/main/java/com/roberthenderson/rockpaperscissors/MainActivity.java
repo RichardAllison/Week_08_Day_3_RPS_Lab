d0048f39f8ca6cf3c028bfe82fe826c42eb6d4ec
Robert Henderson <robert@nterpryz.com>
2018-01-24 14:42:30 +0000

    MVP done

--- app/src/main/java/com/roberthenderson/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/roberthenderson/rockpaperscissors/MainActivity.java
@@ -2,14 +2,19 @@ package com.roberthenderson.rockpaperscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button rockButton;
     Button paperButton;
     Button scissorsButton;
+    TextView winner;
+
+    RockPaperScissors rockPaperScissors;
 
 
     @Override
@@ -17,12 +22,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         rockButton = findViewById(R.id.rockbutton);
         paperButton = findViewById(R.id.paperbutton);
         scissorsButton = findViewById(R.id.scissorsbutton);
+        rockPaperScissors = new RockPaperScissors();
+        winner = findViewById(R.id.winner);
+
+
     }
 
-    public void onRockButtonClicked(View rockButton){
+    public void onButtonClicked(View clickedButton){
+        Button button = (Button) clickedButton;
+        String playerMove = button.getText().toString();
+        String computerMove = rockPaperScissors.getComputerMove();
+        String result = rockPaperScissors.play(playerMove, computerMove);
+        Log.d("Clicked button: ", playerMove + " " + computerMove + " " + result);
+        winner.setText(result);
 
     }
 }
--- app/src/main/java/com/roberthenderson/rockpaperscissors/RockPaperScissors.java
+++ app/src/main/java/com/roberthenderson/rockpaperscissors/RockPaperScissors.java
@@ -1,38 +1,56 @@
 package com.roberthenderson.rockpaperscissors;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * Created by user on 24/01/2018.
  */
 
 public class RockPaperScissors {
 
+    private ArrayList<String> move;
+
+    public RockPaperScissors(){
+        move = new ArrayList<>();
+        move.add("Rock");
+        move.add("Paper");
+        move.add("Scissors");
+    }
+
 
     public String play(String userMove, String computerMove) {
         if (userMove.equals(computerMove)) {
             return "Game is a Draw";
         }
-        if (userMove == "Rock" && computerMove == "Scissors"){
+        if (userMove.equals("Rock") && computerMove.equals("Scissors")){
             return "You Win!!";
         }
-        if (userMove == "Scissors" && computerMove == "Paper"){
+        if (userMove.equals("Scissors") && computerMove.equals("Paper")){
             return "You Win!!";
         }
-        if (userMove == "Paper" && computerMove == "Rock"){
+        if (userMove.equals("Paper") && computerMove.equals("Rock")){
             return "You Win!!";
         }
-
-        if (userMove == "Rock" && computerMove == "Paper"){
+        if (userMove.equals("Rock") && computerMove.equals("Paper")){
             return "You Lose!!";
         }
-        if (userMove == "Scissors" && computerMove == "Rock"){
+        if (userMove.equals("Scissors") && computerMove.equals("Rock")){
             return "You Lose!!";
         }
-        if (userMove == "Paper" && computerMove == "Scissors"){
+        if (userMove.equals("Paper") && computerMove.equals("Scissors")){
             return "You Lose!!";
         }
         return null;
-
-
     }
+
+
+    public String getComputerMove(){
+        Collections.shuffle(move);
+        return this.move.get(0);
+    }
+
+
 
 }
